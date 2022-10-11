7bde545e41312ea6482b6d226b3c9935ff5b134b
MinhHieu259 <mhieu7252@gmail.com>
2022-10-11 23:38:19 +0700

    commit new

--- BT OOP_BAI_2_8/Bai4.rb
+++ BT OOP_BAI_2_8/Bai4.rb
@@ -29,10 +29,12 @@ class Town
     end
 
     def printFamily()
+        puts "------------------"
+        puts "Danh sach cac ho dan trong khu pho"
         for family in Families do
             puts "Dia chi: #{family.address}"
             for member in family.persons do
-                puts "Ten: #{member.name}"
+                puts "Ten: #{member.name} - Tuoi: #{member.age} - Cong viec: #{member.job}"
             end
         end
     end
@@ -42,17 +44,20 @@ town = Town.new()
 print "Nhap so luong gia dinh can nhap: "
 qty_family = gets.to_i
 i = 0
-Persons = []
+Persons = Array.new
 while i < qty_family
+    puts "-------Thong tin gia dinh--------"
     puts "Nhap dia chi: "
     address = gets.to_s
- 
-    Persons.clear
-    
+    if Persons.length > 0
+        Persons.clear
+    end
+    puts "--------------------"
     print "Nhap so nguoi trong gia dinh: "
     num_members = gets.to_i
     j = 0
     while j < num_members
+        puts "--------------------"
         print "Nhap ten: "
         name = gets.to_s
         print "Nhap tuoi: "
